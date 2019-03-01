a29f16b305d16cc12296db9f9215a4a4af0b41ba
vano-gurman <vanogurman@gmail.com>
2019-03-01 21:54:17 +0300

    initial commit

---
+++ src/main/java/com/etherform/caclient/CLApp.java
@@ -0,0 +1,13 @@
+package com.etherform.caclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CLApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CLApp.class, args);
+	}
+
+}
---
+++ src/main/java/com/etherform/caclient/CLController.java
@@ -0,0 +1,13 @@
+package com.etherform.caclient;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CLController {
+
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+}
